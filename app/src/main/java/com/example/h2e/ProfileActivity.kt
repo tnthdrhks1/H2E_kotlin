517a46cd228f2c46e2876565849212694351f644
lsj <dltmdwn021@kangwon.ac.kr>
2020-09-17 23:33:30 +0900

    firestore_load

--- app/src/main/java/com/example/h2e/ProfileActivity.kt
+++ app/src/main/java/com/example/h2e/ProfileActivity.kt
@@ -1,11 +1,41 @@
 package com.example.h2e
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_profile.*
 
-data class ProfileActivity(var name : String? = null, var phoneNumber : String? = null) : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
+    private val TAG = "Firestore"
+    var db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        materialButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        db.collection("user")
+            .get()
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    for (document in task.result!!) {
+                        if(document.id == "rasp"){
+                            var mData = document.data["weight"]
+                            Log.d(TAG, mData.toString())
+                            textView_temp.setText("not error" + "℃")
+                            textView_temp.setText(mData.toString() + "℃")
+                        }
+                    }
+                } else {
+                    textView_temp.setText("error" + "℃")
+                }
+            }
     }
+
 }
---
+++ app/src/main/java/com/example/h2e/person.kt
@@ -0,0 +1,3 @@
+package com.example.h2e
+
+data class Person(var name : String? = null, var phoneNumber : String? = null)
